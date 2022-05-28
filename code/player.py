f9858851f821f74cf7900e53a9ef10f5b507c04d
Oliver-code <olivervelasco05@gmail.com>
2022-05-27 20:57:54 -0500

    Added water and silly unfinished dash

--- code/player.py
+++ code/player.py
@@ -7,6 +7,8 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.frame = 0
+        self.frame_index = -1
+        self.dash_dir = pygame.math.Vector2(0, 0)
 
         self.original_image = pygame.image.load(settings.tile_set2["PlayerIdle"]).convert_alpha()
 
@@ -20,15 +22,20 @@ class Player(pygame.sprite.Sprite):
         self.jump_vel = 8.5
         self.djump_vel = 7
         self.jumps = 2
+        self.max_dash = 1
+        self.dashed = 0
         self.gravity = 0.4
         self.jump_cooldown = True
         self.max_fall = 9
+        self.multiplier = 1
 
         self.dead = False
         self.jumps_counter = 1
         self.grounded = False
         self.velocity = pygame.math.Vector2(0, 0)
-        self.direction = 1
+        self.direction = pygame.math.Vector2(1, 0)
+        self.sprite_facing = 1
+        self.state = 0
 
         self.position = pygame.math.Vector2(pos)
         self.hitbox = mask_rect
@@ -41,12 +48,30 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.velocity.x = 1 * self.speed
-            self.direction = 1
+            self.sprite_facing = 1
+            self.direction.x = 1
         elif keys[pygame.K_LEFT]:
             self.velocity.x = -1 * self.speed
-            self.direction = -1
+            self.sprite_facing = -1
+            self.direction.x = -1
         else:
             self.velocity.x = 0
+            self.direction.x = 0
+
+        if keys[pygame.K_UP]:
+            self.direction.y = 1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+
+        if __main__.key_sys.individual_frame_down[pygame.K_z] and self.state == 0:
+            if self.direction.xy == (0,0):
+                self.dash_dir.xy = (self.sprite_facing,0)
+
+            else:
+                self.dash_dir.xy = self.direction
+            self.state = 1
 
         if (keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]) and self.jump_cooldown == False:
 
@@ -84,7 +109,7 @@ class Player(pygame.sprite.Sprite):
 
 
     def horizontal(self, tiles):
-        self.position.x += self.velocity.x
+        self.position.x += self.velocity.x*self.multiplier
         self.hitbox.topleft = self.position
         for tile in tiles:
             if self.hitbox.colliderect(tile.rect):
@@ -102,7 +127,7 @@ class Player(pygame.sprite.Sprite):
         if not self.grounded:
             self.apply_gravity()
 
-        self.position.y -= self.velocity.y
+        self.position.y -= self.velocity.y*self.multiplier
         self.hitbox.topleft = self.position
 
         for tile in tiles:
@@ -129,8 +154,21 @@ class Player(pygame.sprite.Sprite):
             if pygame.Rect.colliderect(save.rect, self.hitbox) and __main__.key_sys.current_frame_down[pygame.K_s]:
                 save.update_image(True)
                 self.save_position = self.hitbox.topleft
+                return
             else:
                 save.update_image(False)
+
+    def water_collision(self, waters):
+        for water in waters:
+            if pygame.Rect.colliderect(water.rect, self.hitbox):
+                self.jumps_counter = 1
+                self.multiplier = 0.8
+                return
+            else:
+                self.multiplier = 1
+
+
+
 
     def apply_gravity(self):
         self.velocity.y -= self.gravity
@@ -141,17 +179,34 @@ class Player(pygame.sprite.Sprite):
         self.rect.topleft = self.position - self.offset
         self.image = self.original_image
 
-        if self.direction == -1:
+        if self.sprite_facing == -1:
             self.rect.topleft = (self.rect.topleft[0] + 3, self.rect.topleft[1])
             self.image = pygame.transform.flip(self.original_image, True, False)
+
+
+    def manage_dash(self):
+        if self.state == 1:
+
+            if self.frame_index == -1:
+                self.frame_index = self.frame+10
+
+            self.velocity = self.dash_dir*10
+
+        if self.frame >= self.frame_index and not self.frame_index == -1:
+            self.velocity.xy = (0,0)
+            self.state = 0
+            self.frame_index = -1
+
 
     def update(self):
         if not self.dead:
             self.frame += 1
             self.input()
+            self.manage_dash()
             self.check_grounded(__main__.level["tiles"])
             self.block_collision(__main__.level["tiles"])
             self.update_sprite()
             self.save_collision(__main__.level["saves"])
+            self.water_collision(__main__.level["waters"])
             if self.kill_collision(__main__.level["killers"]):
                 self.dead = True
